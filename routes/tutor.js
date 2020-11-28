969f6b442cf52b1f8abf43502dc81e5e678ef234
Rincy Pradeep <rincypradeep15@gmail.com>
2020-11-28 21:47:20 +0530

    week1 task updated

--- routes/tutor.js
+++ routes/tutor.js
@@ -12,7 +12,7 @@ const verifyLogin = (req, res, next) => {
 };
 
 /* GET Landing Page */
-router.get('/', function(req, res, next) {
+router.get('/',verifyLogin, function(req, res, next) {
   res.render('landing-page');
 });
 
@@ -22,8 +22,12 @@ router.get("/tutor_home",verifyLogin,(req,res)=>{
 })
 
 router.get('/tutor_login',(req,res)=>{
-  res.render("tutor/tutor_login",{loginErr: req.session.tutorLoginErr})
+  if (req.session.tutorLoggedIn){
+    res.redirect("/tutor/tutor_home")
+  }else{
+    res.render("tutor/tutor_login",{loginErr: req.session.tutorLoginErr})
   req.session.tutorLoginErr = false;
+}
 })
 
 router.post('/tutor_login',(req,res)=>{
@@ -45,58 +49,58 @@ router.get("/tutor_logout",(req,res)=>{
   res.redirect("/");
 })
 
-router.get("/tutor_profile",(req,res)=>{
+router.get("/tutor_profile",verifyLogin,(req,res)=>{
   
   res.render("tutor/tutor_profile",{tutor:true})
 })
 
-router.get("/edit_profile",(req,res)=>{
+router.get("/edit_profile",verifyLogin,(req,res)=>{
   
   res.render("tutor/edit_profile",{tutor:true})
 })
 
-router.get('/student_details',(req,res)=>{
+router.get('/student_details',verifyLogin,(req,res)=>{
   res.render("tutor/student_details",{tutor:true})
 })
 
-router.get('/add_student',(req,res)=>{
+router.get('/add_student',verifyLogin,(req,res)=>{
   res.render("tutor/add_student",{tutor:true})
 })
 
-router.get('/edit_student',(req,res)=>{
+router.get('/edit_student',verifyLogin,(req,res)=>{
   res.render("tutor/edit_student",{tutor:true})
 })
 
-router.get('/attendance',(req,res)=>{
+router.get('/attendance',verifyLogin,(req,res)=>{
   res.render("tutor/attendance",{tutor:true})
 })
 
-router.get("/assignments",(req,res)=>{
+router.get("/assignments",verifyLogin,(req,res)=>{
   res.render("tutor/assignments",{tutor:true})
 })
 
-router.get("/edit_assignment",(req,res)=>{
+router.get("/edit_assignment",verifyLogin,(req,res)=>{
   res.render("tutor/edit_assignments",{tutor:true})
 })
 
-router.get("/notes",(req,res)=>{
+router.get("/notes",verifyLogin,(req,res)=>{
   res.render("tutor/notes",{tutor:true})
 })
 
-router.get("/edit_notes",(req,res)=>{
+router.get("/edit_notes",verifyLogin,(req,res)=>{
   res.render("tutor/edit_notes",{tutor:true})
 })
 
 
-router.get("/announcements",(req,res)=>{
+router.get("/announcements",verifyLogin,(req,res)=>{
   res.render("tutor/announcements",{tutor:true})
 })
 
-router.get("/events",(req,res)=>{
+router.get("/events",verifyLogin,(req,res)=>{
   res.render("tutor/events",{tutor:true})
 })
 
-router.get("/add_photos",(req,res)=>{
+router.get("/add_photos",verifyLogin,(req,res)=>{
   res.render("tutor/add_photos",{tutor:true})
 })
 module.exports = router;
